d6e03870580affd691a5cb4be3910875213d5339
Mike Battles <mbattles@redhat.com>
2017-02-09 23:44:37 -0500

    adding generator to bin

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ gulp.task('default', ['copyBin','copyLib']);
 
 gulp.task('copyBin',function(){
   var stream = gulp.src('src/a2apigen.js').pipe(gulp.dest('bin'));
+  var stream = gulp.src('src/generator.js').pipe(gulp.dest('bin'));
   return stream;
 });
 
